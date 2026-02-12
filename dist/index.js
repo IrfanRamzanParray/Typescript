13fa623edd53d63467d3122b62e48b3976749d83
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-12 17:59:32 +0530

    setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}, welcome to typescript`;
+}
+const username = 'irfan ramzan parray';
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(person: string):string{
+    return `hello ${person}, welcome to typescript`;
+}
+const username: string = 'irfan ramzan parray';
+console.log(greet(username));
